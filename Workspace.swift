75129dec8ad2efc23460834bccb482ffd07c48c5
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-09-15 16:09:56 +0900

    Workspace 파일 수정

--- Workspace.swift
+++ Workspace.swift
@@ -3,7 +3,9 @@ import ProjectDescription
 let workspace = Workspace(
     name: "PlanIt-Workspace",
     projects: [
-                "Application"
+                "Application",
+                "DesignSystem",
+                "Modules"
               ],
     fileHeaderTemplate: nil,
     additionalFiles: []

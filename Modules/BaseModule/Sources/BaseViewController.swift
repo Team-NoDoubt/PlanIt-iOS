35829f162b37eee2e2a8987d261b670247acbc32
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-09-17 23:02:33 +0900

    BaseViewController

---
+++ Modules/BaseModule/Sources/BaseViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class BaseViewContrller: UIViewController {
+    
+    override func viewDidLoad() {
+        addView()
+        setLayout()
+    }
+
+    func addView() {}
+    func setLayout() {}
+}

95543e67594ef64d32227153d5b2194d9c706f89
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-09-19 22:45:01 +0900

    [ADD] ChangedFlow

---
+++ Application/Sources/Application/Flows/ChangedFlow.swift
@@ -0,0 +1,31 @@
+import UIKit
+import RxFlow
+import RxSwift
+import RxCocoa
+
+class ChangedFlow: Flow {
+
+    private let rootViewController = UINavigationController()
+
+    var root: Presentable {
+        return self.rootViewController
+    }
+
+    func adapt(step: Step) -> Single<Step> {
+        switch step {
+        default:
+            return .just(step)
+        }
+    }
+
+    func navigate(to step: Step) -> FlowContributors {
+        guard let step = step as? AppStep else { return .none }
+
+        switch step {
+        default:
+            return .none
+        }
+    }
+
+
+}

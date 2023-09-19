d4f73f9c2d41b64c4bfea9adb141d672cbda4608
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-09-19 22:43:49 +0900

    sceneDelegate bootstrap 설정

--- Application/Sources/Application/SceneDelegate.swift
+++ Application/Sources/Application/SceneDelegate.swift
@@ -1,15 +1,25 @@
 import UIKit
+import RxFlow
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    var coodintator = FlowCoordinator()
 
-
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = MainViewController()
-        window?.makeKeyAndVisible()
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.windowScene = windowScene
+        let appFlow = AppFlow()
+        self.coodintator.coordinate(flow: appFlow, with: AppStepper())
+        Flows.use(appFlow, when: .created) { [weak self] root in
+            self?.window?.rootViewController = root
+            self?.window?.makeKeyAndVisible()
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {  }

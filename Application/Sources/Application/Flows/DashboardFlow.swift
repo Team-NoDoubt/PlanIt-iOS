a974539934e8f624c2311d3cd1bc4e4d64cce289
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-09-19 22:44:49 +0900

    [ADD] DashboardFlow

--- Application/Sources/Application/Flows/DashboardFlow.swift
+++ Application/Sources/Application/Flows/DashboardFlow.swift
@@ -4,7 +4,7 @@ import UIKit
 
 class DashboardFlow: Flow {
 
-    let rootViewController = UITabBarController()
+    private let rootViewController = UITabBarController()
 
     var root: Presentable {
         return rootViewController
@@ -20,20 +20,34 @@ class DashboardFlow: Flow {
             return .none
         }
     }
-    
-    private func navigateToDashboard() -> FlowContributors {
-        let timeTableStepper = TimeTableStepper()
-        let changedStepper = ChangedStepper()
-        let etcStepper = ETCStepper()
 
-        let timeTableFlow = TimeTableFlow(andSteppr: timeTableStepper)
-        let changedFlow = ChangedFlow(andStepper: changedStepper)
+    private func navigateToDashboard() -> FlowContributors {
+
+        let timeTableFlow = TimeTableFlow()
+        let changedFlow = ChangedFlow()
         let etcFlow = ETCFlow()
 
-        Flows.use(timeTableFlow, changedFlow, etcFlow, when: .created) { [unowned self] (root1: UINavigationController, root2: UINavigationController, root3: UINavigationController  ) in
-            let tabBarItem1 = UITabBarItem(title: "Wishlist", image: UIImage(named: "wishlist"), selectedImage: nil)
-            let tabBarItem2 = UITabBarItem(title: "Watched", image: UIImage(named: "watched"), selectedImage: nil)
-            let tabBarItem3 = UITabBarItem(title: "Trending", image: UIImage(named: "trending"), selectedImage: nil)
+        Flows.use(
+            timeTableFlow,
+            changedFlow,
+            etcFlow,
+            when: .created
+        ) { [unowned self] root1, root2, root3 in
+            let tabBarItem1 = UITabBarItem(
+                title: "Wishlist",
+                image: UIImage(named: "wishlist"),
+                selectedImage: nil
+            )
+            let tabBarItem2 = UITabBarItem(
+                title: "Watched",
+                image: UIImage(named: "watched"),
+                selectedImage: nil
+            )
+            let tabBarItem3 = UITabBarItem(
+                title: "Trending",
+                image: UIImage(named: "trending"),
+                selectedImage: nil
+            )
             root1.tabBarItem = tabBarItem1
             root1.title = "Wishlist"
             root2.tabBarItem = tabBarItem2
@@ -42,14 +56,20 @@ class DashboardFlow: Flow {
             root3.title = "Trending"
 
             self.rootViewController.setViewControllers([root1, root2, root3], animated: false)
+//            self?.rootViewController.selectedIndex = 1
         }
 
-        return .multiple(flowContributors: [.contribute(withNextPresentable: timeTableFlow,
-                                                        withNextStepper: OneStepper(withSingleStep: AppStep.timeTableListAreRequired)),
-                                            .contribute(withNextPresentable: changedFlow,
-                                                        withNextStepper: OneStepper(withSingleStep: AppStep.changedListAreRequired)),
-                                            .contribute(withNextPresentable: etcFlow,
-                                                        withNextStepper: OneStepper(withSingleStep: AppStep.etcListAreRequired))
+        return .multiple(flowContributors: [
+            .contribute(
+                withNextPresentable: timeTableFlow,
+                withNextStepper: OneStepper(withSingleStep: AppStep.timeTableIsRequired)),
+            .contribute(
+                withNextPresentable: changedFlow,
+                withNextStepper: OneStepper(withSingleStep: AppStep.changedSceneIsRequired)),
+            .contribute(
+                withNextPresentable: etcFlow,
+                withNextStepper: OneStepper(withSingleStep: AppStep.etcListAreRequired)
+            )
         
         ])
     }

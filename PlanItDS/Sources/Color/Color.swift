964cec92767b06c171141291ee20fe381a546909
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-09-22 18:55:27 +0900

    [ADD] Color

---
+++ PlanItDS/Sources/Color/Color.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+public extension UIColor {
+    static let main = UIColor(named: "Main")!
+    static let gray900 = UIColor(named: "Gray900")!
+    static let gray800 = UIColor(named: "Gray800")
+    static let gray700 = UIColor(named: "Gray700")
+    static let gray600 = UIColor(named: "Gray600")
+    static let gray500 = UIColor(named: "Gray500")
+    static let gray400 = UIColor(named: "Gray400")
+    static let gray350 = UIColor(named: "Gray350")
+    static let gray300 = UIColor(named: "Gray300")
+
+}

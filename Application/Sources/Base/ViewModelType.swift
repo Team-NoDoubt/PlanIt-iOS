73e6531a0f50e1183dcac70f679f80c39e3b3da1
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-09-23 21:50:17 +0900

    [EDIT] ViewModeltype 수정

--- Application/Sources/Base/ViewModelType.swift
+++ Application/Sources/Base/ViewModelType.swift
@@ -1,9 +1,12 @@
 import Foundation
+import RxSwift
 
 protocol ViewModelType {
 
     associatedtype Input
     associatedtype Output
+
+    var disposeBag: DisposeBag { get set }
 
     func transform(_ input: Input) -> Output
 

0fecd4d7125110dd60f340f4f743bbe87d76811e
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-10-27 18:53:16 +0900

    [ADD] LoginViewModel

--- Application/Sources/Scenes/Login/LoginViewModel.swift
+++ Application/Sources/Scenes/Login/LoginViewModel.swift
@@ -1,10 +1,41 @@
 import Foundation
 import RxFlow
+import RxSwift
+import RxCocoa
+import AuthService
 
-class LoginViewModel {
+class LoginViewModel: ViewModelType, Stepper {
+
+    var steps = PublishRelay<Step>()
+    var disposeBag: DisposeBag = DisposeBag()
+    private let loginUseCase: LoginUseCase
+    var output = Output()
+
+    init(loginUseCase: LoginUseCase) {
+        self.loginUseCase = loginUseCase
+    }
     
-//    var steps: RxRelay.PublishRelay<RxFlow.Step>
+    struct Input {
+        let idText: Driver<String>
+        let passwordText: Driver<String>
+        let loginButtonTap: Driver<Void>
+    }
     
-    
-    
+    struct Output {
+    }
+    func transform(_ input: Input) -> Output {
+        let info = Driver.combineLatest(input.idText, input.passwordText)
+        input.loginButtonTap
+            .asObservable()
+            .withLatestFrom(info)
+            .flatMap { id, password in
+                self.loginUseCase.excute(id: id, password: password)
+                    .andThen(Single.just(AppStep.tabIsRequired))
+            }
+            .bind(to: steps)
+            .disposed(by: disposeBag)
+
+
+        return output
+    }
 }

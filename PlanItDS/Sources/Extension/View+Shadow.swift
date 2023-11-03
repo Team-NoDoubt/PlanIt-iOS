794a36f152955d82294b96250e2fe08d4f4b1c4f
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-11-03 20:14:13 +0900

    viewShadow

--- PlanItDS/Sources/Extension/View+Shadow.swift
+++ PlanItDS/Sources/Extension/View+Shadow.swift
@@ -1,10 +1,16 @@
 import UIKit
 
 public extension UIView {
-    public func shadowTimeTable() {
+    func shadowTimeTable() {
         layer.shadowOffset = CGSize(width: 0, height: 2)
         layer.shadowRadius = 10 / 2.0
         layer.shadowOpacity = Float(0.1)
         layer.shadowColor = UIColor(red: 101/255, green: 101/255, blue: 101/255, alpha: 1).cgColor
+    }
+    func setShadow() {
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOpacity = 0.25
+        self.layer.shadowOffset = CGSize.zero
+        self.layer.shadowRadius = 2
     }
 }

0cc1d1bfd09b1dbfd8cffa3bb25d3d7da6c7973c
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-11-06 10:28:12 +0900

    [ADD] makeDesignSystem

--- Tuist/ProjectDescriptionHelpers/ProjectFrame/DynamicFramework.swift
+++ Tuist/ProjectDescriptionHelpers/ProjectFrame/DynamicFramework.swift
@@ -28,3 +28,37 @@ extension Project {
         )
     }
 }
+
+
+
+import ProjectDescription
+
+extension Project {
+    public static func makeDesignSystem(
+        name: String,
+        platform: Platform,
+        infoPlist: InfoPlist = .default,
+        dependencies: [TargetDependency]
+    ) -> Project {
+        return Project(
+            name: name,
+            targets: [
+                Target(
+                    name: name,
+                    platform: platform,
+                    product: .framework,
+                    bundleId: "\(NodoubtOrganizationName).\(name)",
+                    deploymentTarget: .iOS(
+                        targetVersion: "15.0",
+                        devices: [.iphone, .ipad]
+                    ),
+                    infoPlist: infoPlist,
+                    sources: ["Sources/**"],
+                    resources: ["Resources/**"],
+                    dependencies: dependencies
+                )
+                
+            ]
+        )
+    }
+}
